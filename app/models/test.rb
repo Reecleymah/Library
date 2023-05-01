c45a5cfa515a6928c44161fa8f228517062e6c7a
JORDY IVAN RODRÍGUEZ TORRES <jordyivanrdz_05@hotmail.com>
2023-05-01 14:30:31 -0600

    v1.1.7

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,3 @@
 class Test < ApplicationRecord
+    has_rich_text :content #Indicamos al modelo que tenemos un campo con texto enriquecido
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'library/index'
   #root 'library#index'
   
+  #Rails es un framework que es convencion sobre configuracion,lo cual nos indica que
+  #no debemos hacer muchas configuraciones.
+
   get 'tests/new', to: 'tests#new'
   post 'tests', to: 'tests#create'
   root 'tests#new'
