586f633f6adf89380ccf5070996506ab3c8efb93
JORDY IVAN RODRÍGUEZ TORRES <jordyivanrdz_05@hotmail.com>
2023-05-19 02:14:08 -0600

    Creation Favorites Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+  has_many :favorites
+  has_many :favorite_books, through: :favorites, source: :book
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
